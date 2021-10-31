b62938f9c39021a24e2a5d64ca65d152d2b4a04a
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-10-31 04:45:16 +0100

    added scrolling to select

--- src/components/common/select.tsx
+++ src/components/common/select.tsx
@@ -11,16 +11,21 @@ interface Props {
 function Select(props: Props) {
 	const { value, options, onSelect, color } = props;
 
-	const selected = options.find(option => option.value === value);
-
 	const [show, setShow] = useState(false);
 
+	const selected = options.find(option => option.value === value);
+
+	const len = options.reduce(
+		(acc, { label: { length } }) => (acc < length ? length : acc),
+		0
+	);
+
 	return (
-		<Wrapper onClick={() => setShow(!show)} color={color}>
+		<Wrapper onClick={() => setShow(!show)} color={color} len={len}>
 			<div className="value">{selected.label ?? value}</div>
 
 			{show && (
-				<div className="options">
+				<div className="options scrollar-m">
 					{options.map(option => (
 						<div
 							key={option.value}
@@ -37,9 +42,11 @@ function Select(props: Props) {
 
 const border_radius = "border-radius: 3px;";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ len: number }>`
 	position: relative;
-	background: white;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	margin: 5px;
 	cursor: pointer;
 	min-width: 40px;
@@ -50,32 +57,39 @@ const Wrapper = styled.div`
 		line-height: 15px;
 		${border_radius};
 		border: 1px solid ${props => props.color || "#e2dede"};
+		background-color: white;
 	}
 
 	.options {
+		overflow-y: scroll;
 		position: absolute;
 		top: 40px;
-		width: 100%;
+		width: calc(40px * (${props => props.len} / 4));
+		max-height: 200px;
 	}
 
 	.option {
 		width: 100%;
-		padding: 7px 10px;
+		padding: 7px 8px;
 		margin-bottom: 3px;
 		border: 1px solid gray;
 		background-color: white;
 		${border_radius};
 		transition: all 0.5s;
 	}
-
 	.option:only-child,
 	.option:last-child {
 		margin: 0;
 	}
-
 	.option:hover {
 		background: #d6d1d1;
 		transition: all 0.5s;
+	}
+
+	@media (max-width: 500px) {
+		.options {
+			max-width: 200px;
+		}
 	}
 `;
 
