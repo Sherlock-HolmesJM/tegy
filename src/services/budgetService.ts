b75bbabb6bc9facac00b258b79384cb79a027f42
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-11-03 22:11:06 +0100

    refactor

--- src/services/budgetService.ts
+++ src/services/budgetService.ts
@@ -13,35 +13,39 @@ import { AppDispatch } from "../app/store";
 import { getBatch } from "../utils/batch";
 import { toggledLoading } from "../app/uiSlice";
 
+const getWriter = () => {
+	const db = getFirestore();
+
+	return { writer: writeBatch(db), usersRef: collection(db, "users") };
+};
+
 export const initializeDB = async (user: User) => {
 	try {
-		const db = getFirestore();
-		const batch = writeBatch(db);
+		const { usersRef, writer } = getWriter();
 		const { budgets, selectedBudget } = initialState;
-
-		const usersRef = collection(db, "users");
 
 		const snapshot = await getDoc(doc(usersRef, user.uid));
 		if (snapshot.exists()) return;
 
-		batch.set(doc(usersRef, user.uid), { selectedBudget });
+		writer.set(doc(usersRef, user.uid), { selectedBudget });
 
 		const budget = { ...budgets[0] };
 		delete budget.batches;
 
-		batch.set(doc(usersRef, user.uid, "budgets", budget.id), budget);
+		let pathSegments = [user.uid, "budgets", budget.id];
+
+		writer.set(doc(usersRef, ...pathSegments), budget);
 
 		const batchObj = { ...budgets[0].batches[0] };
 
 		delete batchObj.expense;
 		delete batchObj.income;
 
-		batch.set(
-			doc(usersRef, user.uid, "budgets", budget.id, "batches", batchObj.id),
-			batchObj
-		);
+		pathSegments = [...pathSegments, "batches", batchObj.id];
 
-		batch.commit();
+		writer.set(doc(usersRef, ...pathSegments), batchObj);
+
+		writer.commit();
 	} catch (error) {
 		console.log(error.message);
 	}
@@ -64,9 +68,7 @@ export const addBudget = async (
 		if (item) item = { ...item, amounts: [...item.amounts, ...amounts] };
 		else item = bItem;
 
-		const db = getFirestore();
-		const usersRef = collection(db, "users");
-		const writer = writeBatch(db);
+		const { writer, usersRef } = getWriter();
 
 		const pathSegments = [
 			getCurrentUser().uid,
@@ -90,6 +92,7 @@ export const addBudget = async (
 };
 
 const budgetService = {
+	initializeDB,
 	addBudget
 };
 
