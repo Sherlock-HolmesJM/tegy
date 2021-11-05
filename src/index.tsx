9c63cf2a449a1b7e6f8627e7e1d52090d8133988
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-11-05 07:44:03 +0100

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { initializeApp } from "firebase/app";
+// import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import { store } from "./app/store";
 import { firebaseConfig } from "./utils/config";
-
-// import * as serviceWorker from "./serviceWorker";
-
-// window.border = (color = "red") => `border: 1px solid ${color};`;
-// window.theme = {
-// 	primary: "#32c4c9",
-// 	secondary: "#f33333"
-// };
 
 initializeApp(firebaseConfig);
 
@@ -33,4 +26,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// serviceWorker.register();
