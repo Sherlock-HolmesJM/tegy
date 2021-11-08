560d5eb709bbdf35bbf0c0358618f9b5d6b00ea7
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-11-08 17:35:40 +0100

    added logger

---
+++ src/services/logger.ts
@@ -0,0 +1,12 @@
+import { toast } from "react-toastify";
+import { store } from "../model/store";
+import { setLoading } from "../model/uiSlice";
+
+const log = error => {
+	const m = typeof error === "object" ? error.message : error;
+	console.log(m);
+	toast.error(m);
+	store.dispatch(setLoading(0));
+};
+
+export default log;
