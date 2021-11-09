38318028bfefdf51e05e80273c05cdb8b04ad219
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-11-09 20:16:02 +0100

    budget service on hold

---
+++ src/services/budgetService.ts
@@ -0,0 +1,24 @@
+import { store } from "../model/store";
+import strip from "../utils/striper";
+import { getCurrentUser } from "./authService";
+import { getPathSegments, getWriter } from "./httpService";
+import log from "./logger";
+
+export const createBudget = (budget: Budget) => {
+	try {
+		const { budgetList: blist } = store.getState().budgets;
+
+		const heads: Heads = { batch: budget.batches[0].id, budget: budget.id };
+		const budgetList = [...blist, { id: budget.id, name: budget.name }];
+
+		const writer = getWriter();
+
+		writer.update({ heads, budgetList }, [getCurrentUser().uid]);
+
+		const paths = getPathSegments(strip(heads, ["batch"]));
+
+		writer.set(strip(budget, ["batches"]), paths);
+	} catch (error) {
+		log(error);
+	}
+};
