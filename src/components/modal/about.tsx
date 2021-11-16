106bc6b8b97bbb33557ff2a3055362593f0738fb
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-11-16 22:43:17 +0100

    modified

--- src/components/modal/about.tsx
+++ src/components/modal/about.tsx
@@ -8,7 +8,7 @@ function About() {
 	if (close) return null;
 
 	return (
-		<Container onClick={() => setClose(true)}>
+		<WrapperContainer onClick={() => setClose(true)}>
 			<Wrapper ctheme={window.theme}>
 				<div>Tegy: a simple budget tracker</div>
 
@@ -25,28 +25,48 @@ function About() {
 					you'll never have a messy record.
 				</div>
 
-				<div>And you can have multiple budgets.</div>
+				<div>And you can have multiple budgets with the same account.</div>
 
-				<div>So, what are you waiting for? Give Tegy a try.</div>
+				<div>
+					So, what are you waiting for? Do you want to have a clean budget
+					record?
+				</div>
 			</Wrapper>
-		</Container>
+		</WrapperContainer>
 	);
 }
 
+const WrapperContainer = styled(Container)`
+	z-index: 3;
+`;
+
 const Wrapper = styled(Box)`
 	text-align: justify;
+	gap: 10px;
+	z-index: 3;
 
 	div {
 		color: #1d5970;
+		font-size: 15px;
+		line-height: 20px;
+		font-weight: 500;
+	}
+
+	div:first-child {
+		color: #297e29;
+		font-weight: 700;
+		text-transform: uppercase;
 	}
 
 	div:last-child {
 		color: #297e29;
 	}
 
-	/* span {
-		color: red;
-	} */
+	span {
+		color: #be3e3e;
+		text-decoration: underline;
+		background: #d323233b;
+	}
 `;
 
 export default About;
